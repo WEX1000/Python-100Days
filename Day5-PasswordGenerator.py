807d59fbd33045d867318cd10fa76bbf991f5fca
WEX100 <Jakub.mazur76@wp.pl>
2024-12-30 22:46:03 +0100

    Day5

---
+++ Day5-PasswordGenerator.py
@@ -0,0 +1,27 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+password = []
+passwordstring = ""
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+for i in range(nr_letters):
+    password.append(letters[random.randint(0, 51)])
+for i in range(nr_numbers):
+    password.append(numbers[random.randint(0, 9)])
+for i in range(nr_symbols):
+    password.append(symbols[random.randint(0, 8)])
+
+random.shuffle(password)
+
+for i in range(len(password)):
+    passwordstring += password[i]
+
+print(f'Your password is: {passwordstring}')
+
