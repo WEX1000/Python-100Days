d3986a8df42c959ba90c8508d690e3467255565f
WEX100 <Jakub.mazur76@wp.pl>
2025-01-14 07:06:03 +0100

    Day16

--- Level 2/Day16-CoffeeMachineOOP.py
+++
@@ -1,7 +0,0 @@
-import prettytable
-table1 = ["First", "Second", "Third"]
-table = prettytable.PrettyTable()
-table.add_column("FirstColumn", table1)
-table.add_column("SecondColumn", table1)
-table.align = "l"
-print(table)
