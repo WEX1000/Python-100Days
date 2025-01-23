67ae39e0f4d4fe3c191dd2077524bbf24ee4b65f
WEX100 <Jakub.mazur76@wp.pl>
2025-01-23 22:17:24 +0100

    Day26

--- QuickTestSpace2.py
+++ QuickTestSpace2.py
@@ -1,3 +1,14 @@
-import ufbt
+import pyautogui
+import time
 
-
+# Współrzędne trzech miejsc na ekranie
+positions = [(486, 440), (476, 836), (1001, 563)]
+pyautogui.move()
+try:
+    while True:
+        for pos in positions:
+            pyautogui.move(pos)  # Kliknięcie w pozycję
+            time.sleep(1)  # Opóźnienie między kliknięciami
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("Program zakończony.")
